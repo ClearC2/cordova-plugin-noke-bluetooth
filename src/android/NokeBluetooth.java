e34b60ddb85cade5e4c681c90df2c17dad8946d3
Jacob Allen <jacobrallen1@gmail.com>
2019-01-09 10:33:50 -0600

    chore(init): initial commit

--- src/android/NokeBluetooth.java
+++ src/android/NokeBluetooth.java
@@ -2,6 +2,7 @@ package com.clearc2.cordova.nokebluetooth;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,17 +13,20 @@ public class NokeBluetooth extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("start")) {
-            this.start(callbackContext);
+            start(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void start(CallbackContext callbackContext) {
-        if (true) {
-            callbackContext.success("Start Success");
-        } else {
-            callbackContext.error("Start Failure");
-        }
+    private void start(final CallbackContext callbackContext) {
+		JSONObject succ = new JSONObject();
+		JSONObject fail = new JSONObject();
+		succ.put("message", "success");
+		fail.put("message", "fail");
+    	PluginResult result = new PluginResult(PluginResult.Status.OK, succ);
+		callbackContext.sendPluginResult(result);
+        callbackContext.success(succ);
+        callbackContext.error(fail);
     }
 }
