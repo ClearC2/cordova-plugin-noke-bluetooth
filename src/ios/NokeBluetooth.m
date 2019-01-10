4d202f216e46378527a3dac7e999fa7a83e6834a
Jacob Allen <jacobrallen1@gmail.com>
2019-01-10 12:43:31 -0600

    chore(init): initial commit

--- src/ios/NokeBluetooth.m
+++ src/ios/NokeBluetooth.m
@@ -8,6 +8,15 @@
 
 - (void)initService:(CDVInvokedUrlCommand*)command;
 - (void)bindOnNokeDiscovered:(CDVInvokedUrlCommand*)command;
+- (void)bindOnNokeConnecting:(CDVInvokedUrlCommand*)command;
+- (void)bindOnNokeConnected:(CDVInvokedUrlCommand*)command;
+- (void)bindOnNokeSyncing:(CDVInvokedUrlCommand*)command;
+- (void)bindOnNokeUnlocked:(CDVInvokedUrlCommand*)command;
+- (void)bindOnNokeShutdown:(CDVInvokedUrlCommand*)command;
+- (void)bindOnNokeDisconnected:(CDVInvokedUrlCommand*)command;
+- (void)bindOnDataUploaded:(CDVInvokedUrlCommand*)command;
+- (void)bindOnBluetoothStatusChanged:(CDVInvokedUrlCommand*)command;
+- (void)bindOnNokeError:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation NokeBluetooth
@@ -28,6 +37,42 @@
 
 - (void)bindOnNokeDiscovered:(CDVInvokedUrlCommand*)command {
   NSLog(@"bindOnNokeDiscovered Called.");
+}
+
+- (void)bindOnNokeConnecting:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnNokeConnecting Called.");
+}
+
+- (void)bindOnNokeConnected:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnNokeConnected Called.");
+}
+
+- (void)bindOnNokeSyncing:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnNokeSyncing Called.");
+}
+
+- (void)bindOnNokeUnlocked:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnNokeUnlocked Called.");
+}
+
+- (void)bindOnNokeShutdown:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnNokeShutdown Called.");
+}
+
+- (void)bindOnNokeDisconnected:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnNokeDisconnected Called.");
+}
+
+- (void)bindOnDataUploaded:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnDataUploaded Called.");
+}
+
+- (void)bindOnBluetoothStatusChanged:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnBluetoothStatusChanged Called.");
+}
+
+- (void)bindOnNokeError:(CDVInvokedUrlCommand*)command {
+  NSLog(@"bindOnNokeError Called.");
 }
 
 @end
