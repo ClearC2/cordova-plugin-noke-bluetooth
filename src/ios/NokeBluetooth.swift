5c303ae7cdb1e07565a92c09e36fabd67fe667f3
Jacob Allen <jacobrallen1@gmail.com>
2019-01-21 16:08:18 -0600

    feat(ios): added noke library cocoa pod to ios

--- src/ios/NokeBluetooth.swift
+++ src/ios/NokeBluetooth.swift
@@ -1,3 +1,5 @@
+import NokeMobileLibrary
+
 func consolelog(_ message: String){
     NSLog("%@ - %@", "Noke", message)
 }
@@ -5,9 +7,40 @@ func consolelog(_ message: String){
 @objc(NokeBluetooth) class NokeBluetooth : CDVPlugin {
     func initService(_ command: CDVInvokedUrlCommand) {
         consolelog("initService fired")
-        let result: CDVPluginResult
-        result = CDVPluginResult(status: CDVCommandStatus_ILLEGAL_ACCESS_EXCEPTION)
-        commandDelegate!.send(result, callbackId: command.callbackId)
+        // NokeDeviceManager.shared().setAPIKey("eyJhbGciOiJOT0tFX1BSSVZBVEVfU0FOREJPWCIsInR5cCI6IkpXVCJ9.eyJhbGciOiJOT0tFX1BSSVZBVEVfU0FOREJPWCIsImNvbXBhbnlfdXVpZCI6IjdhZWJiOTgyLWU2YTgtNGMyNi04NzUyLWNhODY2ZjMzYzc2YSIsImlzcyI6Im5va2UuY29tIn0.786e0e298707cd68d91fd85f0da3e803d0cb3cd6") //PRODUCTION
+        // NokeDeviceManager.shared().setLibraryMode(NokeLibraryMode.PRODUCTION)
+        NokeDeviceManager.shared().setAPIKey("eyJhbGciOiJOT0tFX01PQklMRV9TQU5EQk9YIiwidHlwIjoiSldUIn0.eyJhbGciOiJOT0tFX01PQklMRV9TQU5EQk9YIiwiY29tcGFueV91dWlkIjoiN2FlYmI5ODItZTZhOC00YzI2LTg3NTItY2E4NjZmMzNjNzZhIiwiaXNzIjoibm9rZS5jb20ifQ.5eb9b38d6c294e238882703fc7b684e0bba4d28c") //SANDBOX
+        NokeDeviceManager.shared().setLibraryMode(NokeLibraryMode.SANDBOX)
+        NokeDeviceManager.shared().setAllowAllNokeDevices(true)
+        NokeDeviceManager.shared().delegate = self as? NokeDeviceManagerDelegate
+    }
+
+    func nokeDeviceDidUpdateState(to state: NokeDeviceConnectionState, noke: NokeDevice) {
+
+    switch state {
+        case .nokeDeviceConnectionStateDiscovered:
+            consolelog("Nokē Discovered")
+            break
+        case .nokeDeviceConnectionStateConnected:
+            consolelog("Nokē Connected")
+            break
+        case .nokeDeviceConnectionStateSyncing:
+            consolelog("Nokē Syncing")
+            break
+        case .nokeDeviceConnectionStateUnlocked:
+            consolelog("Nokē Unlocked")
+            break
+        case .nokeDeviceConnectionStateDisconnected:
+            consolelog("Nokē Disconnected")
+            break
+        default:
+            consolelog("Unrecognized State")
+            break
+        }
+    }
+
+    func nokeErrorDidOccur(error: NokeDeviceManagerError, message: String, noke: NokeDevice?) {
+      consolelog("nokeErrorDidOccur")
     }
 
     func bindOnNokeDiscovered(_ command: CDVInvokedUrlCommand) {
