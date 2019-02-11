5b32574cfba1c9cb8726fb9f3cf10597e7e811b8
Jacob Allen <jacobrallen1@gmail.com>
2019-02-11 14:38:47 -0600

    feat(ios): bound callback listeners so the UI can tell when a noke device is connected

--- src/android/NokeBluetooth.java
+++ src/android/NokeBluetooth.java
@@ -25,36 +25,36 @@ import com.noke.nokemobilelibrary.NokeServiceListener;
 
 public class NokeBluetooth extends CordovaPlugin {
 
-	public static final String TAG = "Noke";
-	private NokeDeviceManagerService mNokeService = null;
-	// private JSONObject NokeDevices = new JSONObject();
-	private NokeDevice currentLock = null;
+  public static final String TAG = "Noke";
+  private NokeDeviceManagerService mNokeService = null;
+  // private JSONObject NokeDevices = new JSONObject();
+  private NokeDevice currentLock = null;
 
-	CallbackContext onNokeInitCallback = null;
-	CallbackContext onNokeDiscoveredCallback = null;
-	CallbackContext onNokeConnectingCallback = null;
-	CallbackContext onNokeConnectedCallback = null;
-	CallbackContext onNokeSyncingCallback = null;
-	CallbackContext onNokeUnlockedCallback = null;
-	CallbackContext onNokeShutdownCallback = null;
-	CallbackContext onNokeDisconnectedCallback = null;
-	CallbackContext onDataUploadedCallback = null;
-	CallbackContext onBluetoothStatusChangedCallback = null;
-	CallbackContext onErrorCallback = null;
+  CallbackContext onNokeInitCallback = null;
+  CallbackContext onNokeDiscoveredCallback = null;
+  CallbackContext onNokeConnectingCallback = null;
+  CallbackContext onNokeConnectedCallback = null;
+  CallbackContext onNokeSyncingCallback = null;
+  CallbackContext onNokeUnlockedCallback = null;
+  CallbackContext onNokeShutdownCallback = null;
+  CallbackContext onNokeDisconnectedCallback = null;
+  CallbackContext onDataUploadedCallback = null;
+  CallbackContext onBluetoothStatusChangedCallback = null;
+  CallbackContext onErrorCallback = null;
 
-	@Override
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		super.initialize(cordova, webView);
-	}
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+  }
 
   private void initiateNokeService() {
-  	Context context = cordova.getActivity().getApplicationContext();
+    Context context = cordova.getActivity().getApplicationContext();
     Intent nokeServiceIntent = new Intent(context, NokeDeviceManagerService.class);
     context.bindService(nokeServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
-	}
+  }
 
-	private ServiceConnection mServiceConnection = new ServiceConnection() {
-		public void onServiceConnected(ComponentName className, IBinder rawBinder) {
+  private ServiceConnection mServiceConnection = new ServiceConnection() {
+    public void onServiceConnected(ComponentName className, IBinder rawBinder) {
 
       mNokeService = ((NokeDeviceManagerService.LocalBinder) rawBinder).getService(0);
       mNokeService.registerNokeListener(mNokeServiceListener);
@@ -64,177 +64,175 @@ public class NokeBluetooth extends CordovaPlugin {
       if (mNokeService.initialize()) {
         Log.d(TAG, "Noke Service Initialized.");
       } else {
-      	Log.d(TAG, "Noke Service Initialization Failed.");
+        Log.d(TAG, "Noke Service Initialization Failed.");
       }
     }
 
     public void onServiceDisconnected(ComponentName classname) {
-    	Log.d(TAG, "Noke Service Disconnected.");
+      Log.d(TAG, "Noke Service Disconnected.");
       mNokeService = null;
     }
-	};
+  };
 
-	private NokeServiceListener mNokeServiceListener = new NokeServiceListener() {
+  private NokeServiceListener mNokeServiceListener = new NokeServiceListener() {
 
     @Override
     public void onNokeDiscovered(NokeDevice noke) {
-			mNokeService.connectToNoke(noke);
-			if (onNokeDiscoveredCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK, true);
-				result.setKeepCallback(true);
-				onNokeDiscoveredCallback.sendPluginResult(result);
-			}
+      mNokeService.connectToNoke(noke);
+      if (onNokeDiscoveredCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK, true);
+        result.setKeepCallback(true);
+        onNokeDiscoveredCallback.sendPluginResult(result);
+      }
     }
 
     @Override
     public void onNokeConnecting(NokeDevice noke) {
-			if (onNokeConnectingCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK, true);
-				result.setKeepCallback(true);
-				onNokeConnectingCallback.sendPluginResult(result);
-    	}
+      if (onNokeConnectingCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK, true);
+        result.setKeepCallback(true);
+        onNokeConnectingCallback.sendPluginResult(result);
+      }
     }
 
     @Override
     public void onNokeConnected(NokeDevice noke) {
-			if (onNokeConnectingCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK, false);
-				result.setKeepCallback(true);
-				onNokeConnectingCallback.sendPluginResult(result);
-    	}
-			if (onNokeConnectedCallback != null) {
-				JSONObject info = new JSONObject();
-				try {
-					info.put("mac", noke.getMac());
-					info.put("session", noke.getSession());
-					info.put("name", noke.getName());
-					info.put("serial", noke.getSerial());
-					info.put("version", noke.getVersion());
-					info.put("battery", noke.getBattery());
-					info.put("trackingkey", noke.getTrackingKey());
-					info.put("connectionstate", noke.getConnectionState());
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-				PluginResult result = new PluginResult(PluginResult.Status.OK, info);
-				result.setKeepCallback(true);
-				onNokeConnectedCallback.sendPluginResult(result);
-    	}
-			// try {
-			// 	NokeDevices.put(noke.getMac(), noke);
-			// } catch (JSONException e) {
-			// 	e.printStackTrace();
-			// }
-			currentLock = noke;
+      if (onNokeConnectingCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK, false);
+        result.setKeepCallback(true);
+        onNokeConnectingCallback.sendPluginResult(result);
+      }
+      if (onNokeConnectedCallback != null) {
+        JSONObject info = new JSONObject();
+        try {
+          info.put("mac", noke.getMac());
+          info.put("session", noke.getSession());
+          info.put("name", noke.getName());
+          info.put("serial", noke.getSerial());
+          info.put("version", noke.getVersion());
+          info.put("battery", noke.getBattery());
+          info.put("trackingkey", noke.getTrackingKey());
+          info.put("connectionstate", noke.getConnectionState());
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+        PluginResult result = new PluginResult(PluginResult.Status.OK, info);
+        result.setKeepCallback(true);
+        onNokeConnectedCallback.sendPluginResult(result);
+      }
+      // try {
+      // 	NokeDevices.put(noke.getMac(), noke);
+      // } catch (JSONException e) {
+      // 	e.printStackTrace();
+      // }
+      currentLock = noke;
     }
 
     @Override
     public void onNokeSyncing(NokeDevice noke) {
-			if (onNokeSyncingCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK);
-				result.setKeepCallback(true);
-				onNokeSyncingCallback.sendPluginResult(result);
-    	}
+      if (onNokeSyncingCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK);
+        result.setKeepCallback(true);
+        onNokeSyncingCallback.sendPluginResult(result);
+      }
     }
 
     @Override
     public void onNokeUnlocked(NokeDevice noke) {
-			Log.d(TAG, "Noke on unlock - " + noke.getLockState());
-			if (onNokeUnlockedCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK, noke.getLockState());
-				result.setKeepCallback(true);
-				onNokeUnlockedCallback.sendPluginResult(result);
-    	}
+      if (onNokeUnlockedCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK, noke.getLockState());
+        result.setKeepCallback(true);
+        onNokeUnlockedCallback.sendPluginResult(result);
+      }
     }
 
     @Override
     public void onNokeShutdown(NokeDevice noke, Boolean isLocked, Boolean didTimeout) {
-			Log.d(TAG, "Noke Shutdown - " + isLocked + ' ' + didTimeout);
-			if (onNokeShutdownCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK, noke.getMac());
-				result.setKeepCallback(true);
-				onNokeShutdownCallback.sendPluginResult(result);
-    	}
+      if (onNokeShutdownCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK, noke.getMac());
+        result.setKeepCallback(true);
+        onNokeShutdownCallback.sendPluginResult(result);
+      }
     }
 
     @Override
     public void onNokeDisconnected(NokeDevice noke) {
-			if (onNokeDisconnectedCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK, noke.getMac());
-				result.setKeepCallback(true);
-				onNokeDisconnectedCallback.sendPluginResult(result);
-    	}
-			// NokeDevices.remove(noke.getMac());
-			currentLock = null;
+      if (onNokeDisconnectedCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK, noke.getMac());
+        result.setKeepCallback(true);
+        onNokeDisconnectedCallback.sendPluginResult(result);
+      }
+      // NokeDevices.remove(noke.getMac());
+      currentLock = null;
     }
 
     @Override
     public void onDataUploaded(int result, String message) {
-			if (onDataUploadedCallback != null) {
-				PluginResult presult = new PluginResult(PluginResult.Status.OK, "ON DATA UPLOAD PAYLOAD");
-				presult.setKeepCallback(true);
-				onDataUploadedCallback.sendPluginResult(presult);
-    	}
+      if (onDataUploadedCallback != null) {
+        PluginResult presult = new PluginResult(PluginResult.Status.OK);
+        presult.setKeepCallback(true);
+        onDataUploadedCallback.sendPluginResult(presult);
+      }
     }
 
     @Override
     public void onBluetoothStatusChanged(int bluetoothStatus) {
-			if (onBluetoothStatusChangedCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK, "ON BLUETOOTH STATUS CHANGE PAYLOAD");
-				result.setKeepCallback(true);
-				onBluetoothStatusChangedCallback.sendPluginResult(result);
-    	}
+      if (onBluetoothStatusChangedCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK);
+        result.setKeepCallback(true);
+        onBluetoothStatusChangedCallback.sendPluginResult(result);
+      }
     }
 
     @Override
     public void onError(NokeDevice noke, int error, String message) {
-			if (onErrorCallback != null) {
-				JSONObject info = new JSONObject();
-				try {
-					info.put("code", error);
-					info.put("message", message);
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-				PluginResult result = new PluginResult(PluginResult.Status.OK, info);
-				result.setKeepCallback(true);
-				onErrorCallback.sendPluginResult(result);
-    	}
+      if (onErrorCallback != null) {
+        JSONObject info = new JSONObject();
+        try {
+          info.put("code", error);
+          info.put("message", message);
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+        PluginResult result = new PluginResult(PluginResult.Status.OK, info);
+        result.setKeepCallback(true);
+        onErrorCallback.sendPluginResult(result);
+      }
     }
   };
 
-	@Override
+  @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
     if (action.equals("initService")) {
-			if (mNokeService == null) {
-				initService();
-			}
-			if (onNokeInitCallback != null) {
-				PluginResult result = new PluginResult(PluginResult.Status.OK);
-				result.setKeepCallback(true);
-				onNokeInitCallback.sendPluginResult(result);
-			}
+      if (mNokeService == null) {
+        initService();
+      }
+      if (onNokeInitCallback != null) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK);
+        result.setKeepCallback(true);
+        onNokeInitCallback.sendPluginResult(result);
+      }
       return true;
     }
 
-		if (action.equals("sendCommands")) {
-			String id = args.getString(0);
-			String commands = args.getString(1);
-			if (currentLock != null) {
-				Log.d(TAG, "RECIEVED THIS COMMAND STRING FROM API - SENDING TO DEVICE: " + commands);
-				currentLock.sendCommands(commands);
-				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, id));
-    	} else {
-				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, id));
-			}
-			return true;
-		}
+    if (action.equals("sendCommands")) {
+      String id = args.getString(0);
+      String commands = args.getString(1);
+      if (currentLock != null) {
+        Log.d(TAG, "RECIEVED THIS COMMAND STRING FROM API - SENDING TO DEVICE: " + commands);
+        currentLock.sendCommands(commands);
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, id));
+      } else {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, id));
+      }
+      return true;
+    }
 
-		if (action.equals("bindOnNokeInit")) {
-			bindOnNokeInit(callbackContext);
-			return true;
-		}
+    if (action.equals("bindOnNokeInit")) {
+      bindOnNokeInit(callbackContext);
+      return true;
+    }
 
     if (action.equals("bindOnNokeDiscovered")) {
       bindOnNokeDiscovered(callbackContext);
@@ -290,50 +288,50 @@ public class NokeBluetooth extends CordovaPlugin {
   };
 
   private void initService() {
-		initiateNokeService();
+    initiateNokeService();
   };
 
-	private void bindOnNokeInit(final CallbackContext callbackContext) {
-		onNokeInitCallback = callbackContext;
-	}
+  private void bindOnNokeInit(final CallbackContext callbackContext) {
+    onNokeInitCallback = callbackContext;
+  }
 
   private void bindOnNokeDiscovered(final CallbackContext callbackContext) {
-		onNokeDiscoveredCallback = callbackContext;
+    onNokeDiscoveredCallback = callbackContext;
   };
 
   private void bindOnNokeConnecting(final CallbackContext callbackContext) {
-		onNokeConnectingCallback = callbackContext;
+    onNokeConnectingCallback = callbackContext;
   };
 
   private void bindOnNokeConnected(final CallbackContext callbackContext) {
-		onNokeConnectedCallback = callbackContext;
+    onNokeConnectedCallback = callbackContext;
   };
 
   private void bindOnNokeSyncing(final CallbackContext callbackContext) {
-		onNokeSyncingCallback = callbackContext;
+    onNokeSyncingCallback = callbackContext;
   };
 
   private void bindOnNokeUnlocked(final CallbackContext callbackContext) {
-		onNokeUnlockedCallback = callbackContext;
+    onNokeUnlockedCallback = callbackContext;
   };
 
   private void bindOnNokeShutdown(final CallbackContext callbackContext) {
-		onNokeShutdownCallback = callbackContext;
+    onNokeShutdownCallback = callbackContext;
   };
 
   private void bindOnNokeDisconnected(final CallbackContext callbackContext) {
-		onNokeDisconnectedCallback = callbackContext;
+    onNokeDisconnectedCallback = callbackContext;
   };
 
   private void bindOnDataUploaded(final CallbackContext callbackContext) {
-		onDataUploadedCallback = callbackContext;
+    onDataUploadedCallback = callbackContext;
   };
 
   private void bindOnBluetoothStatusChanged(final CallbackContext callbackContext) {
-		onBluetoothStatusChangedCallback = callbackContext;
+    onBluetoothStatusChangedCallback = callbackContext;
   };
 
   private void bindOnNokeError(final CallbackContext callbackContext) {
-		onErrorCallback = callbackContext;
+    onErrorCallback = callbackContext;
   };
 }
--- src/ios/NokeBluetooth.swift
+++ src/ios/NokeBluetooth.swift
@@ -7,7 +7,17 @@ func consolelog(_ message: String){
 @objc(NokeBluetooth) class NokeBluetooth : CDVPlugin, NokeDeviceManagerDelegate {
 
   var currentNoke : NokeDevice?
-  var onDiscoId : String?
+  var onNokeInitCallback : String?
+  var onNokeDiscoveredCallback : String?
+  var onNokeConnectingCallback : String?
+  var onNokeConnectedCallback : String?
+  var onNokeSyncingCallback : String?
+  var onNokeUnlockedCallback : String?
+  var onNokeShutdownCallback : String?
+  var onNokeDisconnectedCallback : String?
+  var onDataUploadedCallback : String?
+  var onBluetoothStatusChangedCallback : String?
+  var onErrorCallback : String?
 
   func initService(_ command: CDVInvokedUrlCommand) {
     // NokeDeviceManager.shared().setAPIKey("eyJhbGciOiJOT0tFX1BSSVZBVEVfU0FOREJPWCIsInR5cCI6IkpXVCJ9.eyJhbGciOiJOT0tFX1BSSVZBVEVfU0FOREJPWCIsImNvbXBhbnlfdXVpZCI6IjdhZWJiOTgyLWU2YTgtNGMyNi04NzUyLWNhODY2ZjMzYzc2YSIsImlzcyI6Im5va2UuY29tIn0.786e0e298707cd68d91fd85f0da3e803d0cb3cd6") //PRODUCTION
@@ -16,41 +26,91 @@ func consolelog(_ message: String){
     NokeDeviceManager.shared().setLibraryMode(NokeLibraryMode.SANDBOX)
     NokeDeviceManager.shared().setAllowAllNokeDevices(true)
     NokeDeviceManager.shared().delegate = self
+    if (onNokeInitCallback != nil) {
+      let result: CDVPluginResult
+      result = CDVPluginResult(status: CDVCommandStatus_OK)
+      result.setKeepCallbackAs(true)
+      commandDelegate!.send(result, callbackId: onNokeInitCallback)
+    }
   }
 
   func nokeDeviceDidUpdateState(to state: NokeDeviceConnectionState, noke: NokeDevice) {
     switch state {
     case .nokeDeviceConnectionStateDiscovered:
-      let result: CDVPluginResult
-      let jsonObject: [String: Any] = [
-          "type_id": 1,
-          "model_id": 1,
-          "transfer": [
-              "startDate": "10/04/2015 12:45",
-              "endDate": "10/04/2015 16:00"
-          ]
-      ]
-      result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: jsonObject)
-      result.setKeepCallbackAs(true)
-      commandDelegate!.send(result, callbackId: onDiscoId)
+      if (onNokeDiscoveredCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeDiscoveredCallback)
+      }
       NokeDeviceManager.shared().stopScan()
+      if (onNokeConnectingCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: true)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeConnectingCallback)
+      }
       NokeDeviceManager.shared().connectToNokeDevice(noke)
       break
     case .nokeDeviceConnectionStateConnected:
+      if (onNokeConnectingCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: false)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeConnectingCallback)
+      }
+      if (onNokeConnectedCallback != nil) {
+        let result: CDVPluginResult
+        let info: [String: Any] = [
+          "mac": noke.mac,
+          "session": noke.session!,
+          "name": noke.name,
+          "serial": noke.serial,
+          "version": noke.version,
+          "battery": noke.battery,
+          "trackingkey": noke.trackingKey
+        ]
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: info)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeConnectedCallback)
+      }
       currentNoke = noke
       break
     case .nokeDeviceConnectionStateSyncing:
-      consolelog("Nokē Syncing")
+      if (onNokeSyncingCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeSyncingCallback)
+      }
       break
     case .nokeDeviceConnectionStateUnlocked:
+      if (onNokeUnlockedCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: noke.lockState.rawValue)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeUnlockedCallback)
+      }
       NokeDeviceManager.shared().startScanForNokeDevices()
       break
     case .nokeDeviceConnectionStateDisconnected:
+      if (onNokeDisconnectedCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: noke.mac)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeDisconnectedCallback)
+      }
       NokeDeviceManager.shared().cacheUploadQueue()
       NokeDeviceManager.shared().startScanForNokeDevices()
       currentNoke = nil
       break
     default:
+      if (onNokeConnectingCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: true)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onNokeConnectingCallback)
+      }
       consolelog("UNRECOGNIZED NOKE DEVICE STATE")
       break
     }
@@ -59,11 +119,21 @@ func consolelog(_ message: String){
   func bluetoothManagerDidUpdateState(state: NokeManagerBluetoothState) {
     switch (state) {
     case NokeManagerBluetoothState.poweredOn:
-      consolelog("NOKE BLUETOOTH MANAGER ON")
+      if (onBluetoothStatusChangedCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: true)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onBluetoothStatusChangedCallback)
+      }
       NokeDeviceManager.shared().startScanForNokeDevices()
       break
     case NokeManagerBluetoothState.poweredOff:
-      consolelog("NOKE BLUETOOTH MANAGER OFF")
+      if (onBluetoothStatusChangedCallback != nil) {
+        let result: CDVPluginResult
+        result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: false)
+        result.setKeepCallbackAs(true)
+        commandDelegate!.send(result, callbackId: onBluetoothStatusChangedCallback)
+      }
       break
     default:
       consolelog("UNRECOGNIZED BLUETOOTH MANAGER STATE")
@@ -72,59 +142,78 @@ func consolelog(_ message: String){
   }
 
   func nokeDeviceDidShutdown(noke: NokeDevice, isLocked: Bool, didTimeout: Bool) {
-    // consolelog("noke device did shutdown")
+    if (onNokeShutdownCallback != nil) {
+      let result: CDVPluginResult
+      result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: noke.mac)
+      result.setKeepCallbackAs(true)
+      commandDelegate!.send(result, callbackId: onNokeShutdownCallback)
+    }
+    currentNoke = nil
   }
 
   func didUploadData(result: Int, message: String) {
-    // consolelog("noke did upload data")
-  }
-
-  func bindOnNokeInit(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeInit fired")
+    if (onDataUploadedCallback != nil) {
+      let result: CDVPluginResult
+      result = CDVPluginResult(status: CDVCommandStatus_OK)
+      result.setKeepCallbackAs(true)
+      commandDelegate!.send(result, callbackId: onDataUploadedCallback)
+    }
   }
 
   func nokeErrorDidOccur(error: NokeDeviceManagerError, message: String, noke: NokeDevice?) {
-    // consolelog("nokeErrorDidOccur")
+    if (onErrorCallback != nil) {
+      let result: CDVPluginResult
+      let info: [String: Any] = [
+          "code": 0,
+          "message": message
+      ]
+      result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: info)
+      result.setKeepCallbackAs(true)
+      commandDelegate!.send(result, callbackId: onErrorCallback)
+    }
+  }
+
+  func bindOnNokeInit(_ command: CDVInvokedUrlCommand) {
+    onNokeInitCallback = command.callbackId
   }
 
   func bindOnNokeDiscovered(_ command: CDVInvokedUrlCommand) {
-    onDiscoId = command.callbackId
-    // consolelog("bindOnNokeDiscovered fired")
+    onNokeDiscoveredCallback = command.callbackId
   }
 
   func bindOnNokeConnecting(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeConnecting fired")
+    onNokeConnectingCallback = command.callbackId
   }
 
   func bindOnNokeConnected(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeConnected fired")
+    onNokeConnectedCallback = command.callbackId
   }
 
   func bindOnNokeSyncing(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeSyncing fired")
+    onNokeSyncingCallback = command.callbackId
   }
 
   func bindOnNokeUnlocked(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeUnlocked fired")
+    onNokeUnlockedCallback = command.callbackId
   }
 
   func bindOnNokeShutdown(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeShutdown fired")
+    onNokeShutdownCallback = command.callbackId
   }
 
   func bindOnNokeDisconnected(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeDisconnected fired")
+    onNokeDiscoveredCallback = command.callbackId
   }
 
   func bindOnDataUploaded(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnDataUploaded fired")
+    onDataUploadedCallback = command.callbackId
   }
 
   func bindOnBluetoothStatusChanged(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnBluetoothStatusChanged fired")
+    onBluetoothStatusChangedCallback = command.callbackId
   }
 
   func bindOnNokeError(_ command: CDVInvokedUrlCommand) {
-    // consolelog("bindOnNokeError fired")
+    onErrorCallback = command.callbackId
   }
 }
